bbb80164ef12ef129cec9e50a7d10f51a6a1910a
KVVaisakh <36007302+KVVaisakh@users.noreply.github.com>
2018-06-25 00:04:20 +0530

    Update views.py

--- welcome/views.py
+++ welcome/views.py
@@ -5,6 +5,6 @@ from .forms import UrlForm
 def enter(request):
     form=UrlForm(request.POST or None)
     if form.is_valid():
-        instance=form.save(commit=False)
+        instance=form.save(commit=True)
     context={"today" : datetime.datetime.now().date(),"form":form}
     return render(request, "EnterUrl.html", context)
