c50240118487ee15d88730647ff64ad922aa17e4
KVVaisakh <36007302+KVVaisakh@users.noreply.github.com>
2018-06-24 23:53:42 +0530

    Add files via upload

---
+++ welcome/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Url(models.Model):
+    url=models.CharField(max_length=2000)
+    date=models.DateTimeField(auto_now_add=True,auto_now=False)
+    update=models.DateTimeField(auto_now_add=False,auto_now=True)
+
+    def __str__(self):
+        return self.url
---
+++ welcome/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+import datetime
+from .forms import UrlForm
+
+def enter(request):
+    form=UrlForm(request.POST or None)
+    if form.is_valid():
+        instance=form.save(commit=False)
+    context={"today" : datetime.datetime.now().date(),"form":form}
+    return render(request, "EnterUrl.html", context)
