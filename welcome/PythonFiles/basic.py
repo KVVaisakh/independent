7db3cd55de11ee94c4b5f97091d881212f74420e
KVVaisakh <36007302+KVVaisakh@users.noreply.github.com>
2018-06-25 23:08:25 +0530

    Add files via upload

---
+++ welcome/PythonFiles/basic.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_rss_feed1(website_url):
+    if website_url is None:
+        print("URL should not be null")
+        return "0"
+    else:
+        href=""
+        source_code = requests.get(website_url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text,"lxml")
+        for link in soup.find_all("link", {"type" : "application/rss+xml"}):
+            href = link.get('href')
+            print("RSS feed for " + website_url + "is -->" + str(href))
+            break
+        if(href!=""):
+            return str(href)
+        else:
+            return "0"
--- welcome/views.py
+++ welcome/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 import datetime
 from .forms import UrlForm
+from .PythonFiles.bbc import get_rss_feed2
+from .PythonFiles.basic import get_rss_feed1
+from .PythonFiles.rss import MakeXml
+from .PythonFiles.reddit import MakeXml2
+import webbrowser
+import os
 
 def enter(request):
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     form=UrlForm(request.POST or None)
     if form.is_valid():
-        instance=form.save(commit=True)
+        instance=form.save(commit=False)
+        RssFeed=get_rss_feed1(instance.url)
+        if RssFeed=="0":
+            RssFeed=get_rss_feed2(instance.url)
+        if RssFeed=="0":
+            RssFeed=instance.url+".rss"
+            name=MakeXml2(RssFeed)
+        else:
+            name=MakeXml(RssFeed)
+        instance.save()
+        webbrowser.open('file://' + os.path.realpath("welcome/files/"+name+".html"))
     context={"today" : datetime.datetime.now().date(),"form":form}
     return render(request, "EnterUrl.html", context)
